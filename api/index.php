0e11933d54f851c971915329144313523de446a9
Wesley Willian <wesleywilliancg@gmail.com>
2024-01-07 20:26:51 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,10 +1,17 @@
 <?php
 // api.php
 
+// Habilita o CORS para todas as origens
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS"); // Adiciona OPTIONS aqui
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
+// Verifica se a requisição é um OPTIONS (preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 // Inicializa a sessão
 session_start();
